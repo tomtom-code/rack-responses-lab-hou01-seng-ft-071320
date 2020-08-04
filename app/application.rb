66ddc1338f2212a404ff30ed972ad0f6df6ffc63
Yu Hao (Tom) Lai <tomlai8882000@hotmail.com>
2020-08-04 04:02:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,7 @@ class Application
 
     def call(env)
         resp = Rack::Response.new
-        time = Time.now
+        
         if Time.now.hour < 12
             resp.write "Good Morning!"
         else
